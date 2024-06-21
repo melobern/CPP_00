0376bb59679d18a10534016d5b49c6ee095eea60
Melodie Bernard (from school) <melodie.bernard123@yahoo.fr>
2024-06-21 14:37:35 +0200

    added -std=c++98 on the Makefile

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/21 13:50:57 by mbernard          #+#    #+#             */
+/*   Updated: 2024/06/21 13:51:00 by mbernard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < ac; i++)
+	{
+		for (int j = 0; av[i][j]; j++)
+			std::cout << (char)toupper(av[i][j]);
+	}
+	std::cout << std::endl;
+	return (0);
+}
