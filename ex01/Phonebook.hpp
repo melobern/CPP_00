adacb8fa8c9470bffe285f799610a7fbd18ef0d6
melodie <melodie.bernard123@yahoo.fr>
2024-07-26 20:01:38 +0200

    began parsing correctly, might change the function to check if its digits only to static again since I must check only one number once I checked the length

--- ex01/Phonebook.hpp
+++ ex01/Phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 08:43:15 by mbernard          #+#    #+#             */
-/*   Updated: 2024/07/25 18:26:38 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/07/26 19:55:15 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ class PhoneBook
 		Contact d_contacts[8];
 		int	d_count;
 		bool	has_contacts;
-		void	private_searchContact(void);
+		void	private_searchContact(int index);
 		void	private_replaceContact(void);
 		void	private_displayTable(void);
 		void	private_displayContactDetails(void);
