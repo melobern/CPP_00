59b8f309e7b3c87fbce7471629b960083e70574b
mbernard <melodie.bernard123@yahoo.fr>
2024-07-27 18:57:03 +0200

    cpplinetted megaphone

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 13:50:57 by mbernard          #+#    #+#             */
-/*   Updated: 2024/07/27 18:53:45 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/07/27 18:55:24 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int    main(int ac, char **av) {
     }
     for (int i = 1; i < ac; i++) {
         for (int j = 0; av[i][j]; j++)
-            std::cout << static_cast<char>toupper(av[i][j]);
+            std::cout << static_cast<char>(toupper(av[i][j]));
     }
     std::cout << std::endl;
     return (0);
