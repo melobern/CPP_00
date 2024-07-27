a853008d6c4d55589284f593689045d4e62f0cc9
mbernard <mbernard@student.42lyon.fr>
2024-07-27 21:58:54 +0200

    Display TimeStamp ok

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/27 18:26:04 by mbernard          #+#    #+#             */
-/*   Updated: 2024/07/27 20:17:12 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/07/27 21:57:07 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,21 @@
 #include <ctime>
 #include "../ex02/Account.hpp"
 
-void    Account::Account(void)
-    :initial_deposit(0); {
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+void    Account::Account(int initial_deposit)
+    :_accountIndex(_nbAccounts++),
+     _amount(initial_deposit),
+     _nbDeposits(0),
+     _nbWithdrawals(0) {
+    Account::_displayTimestamp();
+    std::cout << "index:" << this->_accountIndex << ";";
+    std::cout << "amount:" << initial_deposit << ";";
+    std::cout << "created" << std::endl;
+    _totalAmount += this->amount;
     return;
 }
 
@@ -58,4 +71,10 @@ void    displayStatus(void) const {
 }
 
 staticvoid   _displayTimestamp(void) {
+    char           timestamp[16];
+    std::time_t    time = std::time(0);
+    std::tm        *locatime = std::localtime(&time);
+
+    std::strftime(timestamp, sizeof(timestamp), "%Y%m%d_%H%M%S", localtime);
+    std::cout << "[" << timestamp << "] ";
 }
