5f21c80c648b5a8816ac317b6b6c687aa7b530dc
mbernard <mbernard@student.42lyon.fr>
2024-07-27 19:07:53 +0200

    cpplinetted megaphone

--- ex01/includes/Contact.hpp
+++ ex01/includes/Contact.hpp
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/* Copyright 2024 <mbernard>************************************************* */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   Contact.hpp                                        :+:      :+:    :+:   */
@@ -6,41 +6,40 @@
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 08:43:15 by mbernard          #+#    #+#             */
-/*   Updated: 2024/07/26 21:32:00 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/07/27 19:05:15 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CONTACT_HPP
-# define CONTACT_HPP
+#ifndef EX01_INCLUDES_CONTACT_HPP_
+#define EX01_INCLUDES_CONTACT_HPP_
 
 # include <iostream>
 # include <iomanip>
 # include <string>
 # include <cstdlib>
 
-class Contact
-{
-	public:
-		Contact(void);
-		~Contact(void);
-		/*	SETTLERS	*/
-		void	setFirstname(void);
-		void	setLastname(void);
-		void	setNickname(void);
-		void	setPhone(void);
-		void	setSecret(void);
-		/*	GETTERS		*/
-		std::string	getFirstname(void) const;
-		std::string	getLastname(void) const;
-		std::string	getNickname(void) const;
-		std::string	getPhone(void) const;
-		std::string	getSecret(void) const;
-	private:
-		std::string	m_firstname;
-		std::string	m_lastname;
-		std::string	m_nickname;
-		std::string	m_phone;
-		std::string	m_secret;
+class Contact {
+ public:
+        Contact(void);
+        ~Contact(void);
+        /*    SETTLERS    */
+        void    setFirstname(void);
+        void    setLastname(void);
+        void    setNickname(void);
+        void    setPhone(void);
+        void    setSecret(void);
+        /*    GETTERS        */
+        std::string    getFirstname(void) const;
+        std::string    getLastname(void) const;
+        std::string    getNickname(void) const;
+        std::string    getPhone(void) const;
+        std::string    getSecret(void) const;
+ private:
+        std::string    m_firstname;
+        std::string    m_lastname;
+        std::string    m_nickname;
+        std::string    m_phone;
+        std::string    m_secret;
 };
 
-#endif
+#endif  // EX01_INCLUDES_CONTACT_HPP_
--- ex01/includes/Phonebook.hpp
+++ ex01/includes/Phonebook.hpp
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/* Copyright 2024 <mbernard>************************************************* */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   Phonebook.hpp                                      :+:      :+:    :+:   */
@@ -6,33 +6,32 @@
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 08:43:15 by mbernard          #+#    #+#             */
-/*   Updated: 2024/07/27 12:29:30 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/07/27 19:07:25 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHONEBOOK_HPP
-# define PHONEBOOK_HPP
+#ifndef EX01_INCLUDES_PHONEBOOK_HPP_
+#define EX01_INCLUDES_PHONEBOOK_HPP_
 
 # include <iostream>
 # include <iomanip>
 # include <string>
 # include "Contact.hpp"
 
-class PhoneBook
-{
-	public:
-		PhoneBook(void);
-		~PhoneBook(void);
-		void	addContact(void);
-		void	searchContact(void);
-	private:
-		Contact d_contacts[8];
-		int	d_count;
-		bool	has_contacts;
-		void	private_searchContact(int index);
-		void	private_displayTable(void);
-		void	private_displayContact(int index);
-		void	private_displayElement(std::string element);
+class PhoneBook {
+ public:
+        PhoneBook(void);
+        ~PhoneBook(void);
+        void    addContact(void);
+        void    searchContact(void);
+ private:
+        Contact d_contacts[8];
+        int    d_count;
+        bool    has_contacts;
+        void    private_searchContact(int index);
+        void    private_displayTable(void);
+        void    private_displayContact(int index);
+        void    private_displayElement(std::string element);
 };
 
-#endif
+#endif  // EX01_INCLUDES_PHONEBOOK_HPP_
