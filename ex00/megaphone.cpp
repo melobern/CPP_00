beb5e1af1c0b590344c550e9795642eecce0983d
mbernard <mbernard@student.42lyon.fr>
2024-07-27 18:54:43 +0200

    cpplinetted megaphone

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/* Copyright 2024 <mbernard>************************************************* */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
@@ -6,24 +6,21 @@
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 13:50:57 by mbernard          #+#    #+#             */
-/*   Updated: 2024/06/21 13:51:00 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/07/27 18:53:45 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int	main(int ac, char **av)
-{
-	if (ac == 1)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (0);
-	}
-	for (int i = 1; i < ac; i++)
-	{
-		for (int j = 0; av[i][j]; j++)
-			std::cout << (char)toupper(av[i][j]);
-	}
-	std::cout << std::endl;
-	return (0);
+int    main(int ac, char **av) {
+    if (ac == 1) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    }
+    for (int i = 1; i < ac; i++) {
+        for (int j = 0; av[i][j]; j++)
+            std::cout << static_cast<char>toupper(av[i][j]);
+    }
+    std::cout << std::endl;
+    return (0);
 }
