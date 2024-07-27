85e4e8d7a07dcd4b6bade5e5c903050fa5529b29
mbernard <mbernard@student.42lyon.fr>
2024-07-27 13:13:54 +0200

    ex01 finished, files are organized in their place, in their own folder

---
+++ ex01/src/main.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/25 11:03:25 by mbernard          #+#    #+#             */
+/*   Updated: 2024/07/26 21:50:20 by mbernard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "Phonebook.hpp"
+
+int	main(void)
+{
+	PhoneBook	phonebook;
+	std::string	input;
+
+	while (true)
+	{
+		std::cout << "Enter command : ";
+		std::getline(std::cin, input);
+		if (std::cin.eof())
+		{
+			std::cout << std::endl << "Reached the End-of-File (Ctrl + D). Exiting..." << std::endl;
+			break ;
+		}
+		if (input.empty())
+			continue ;
+		if (input == "ADD")
+			phonebook.addContact();
+		else if (input == "SEARCH")
+			phonebook.searchContact();
+		else if (input == "EXIT")
+			break ;
+		else
+			continue;
+	}
+	return (0);
+}
+
