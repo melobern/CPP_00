a52319190d30a9174605b2de4456897e2bb7261b
mbernard <melodie.bernard123@yahoo.fr>
2024-07-27 19:54:37 +0200

    cpplinetted Contact.cpp finished

--- ex01/src/Contact.cpp
+++ ex01/src/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 08:44:17 by mbernard          #+#    #+#             */
-/*   Updated: 2024/07/27 19:45:31 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/07/27 19:53:39 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,11 @@
 /*    CONSTRUCTOR && DESTRUCTOR    */
 
 Contact::Contact(void)
-    : m_firstname(""), m_lastname(""), m_nickname(""), m_phone(""), m_secret("") {
+    : m_firstname(""),
+    m_lastname(""),
+    m_nickname(""),
+    m_phone(""),
+    m_secret("") {
     return;
 }
 
@@ -50,11 +54,13 @@ static bool is_full_digits(std::string str) {
 
     while (str[x]) {
         if (x > 9) {
-            std::cout << "Sorry, the phone number must contains 8 or 10 digits." << std::endl;
+            std::cout << "Sorry, the phone number must contain 8 or 10 digits.";
+            std::cout << std::endl;
             return (0);
         }
         if (!std::isdigit(str[x])) {
-            std::cout << "Sorry, this field must contains digits only." << std::endl;
+            std::cout << "Sorry, this field must contain digits only.";
+            std::cout << std::endl;
             return (0);
         }
         x++;
@@ -67,7 +73,8 @@ static bool is_full_letters(std::string str, std::string label) {
 
     while (str[x]) {
         if (!std::isalpha(str[x]) && !std::isspace(str[x]) && str[x] != '-') {
-            std::cout << "Sorry, the " << label << " must contains letters only." << std::endl;
+            std::cout << "Sorry, " << label << " must contain letters only.";
+            std::cout << std::endl;
             return (0);
         }
         x++;
@@ -105,7 +112,9 @@ void    Contact::setLastname(void) {
         std::cout << "Enter last name : ";
         std::getline(std::cin, input);
         if (std::cin.eof()) {
-            std::cout << std::endl << "Reached the End-of-File (Ctrl + D). Exiting..." << std::endl;
+            std::cout << std::endl;
+            std::cout << "Reached the End-of-File (Ctrl + D). Exiting...";
+            std::cout << std::endl;
             exit(0);
         }
         if (input.empty())
@@ -124,7 +133,9 @@ void    Contact::setNickname(void) {
         std::cout << "Enter nickname : ";
         std::getline(std::cin, input);
         if (std::cin.eof()) {
-            std::cout << std::endl << "Reached the End-of-File (Ctrl + D). Exiting..." << std::endl;
+            std::cout << std::endl;
+            std::cout << "Reached the End-of-File (Ctrl + D). Exiting...";
+            std::cout << std::endl;
             exit(0);
         }
         if (input.empty())
@@ -142,7 +153,9 @@ void    Contact::setPhone(void) {
         std::cout << "Enter phone number : ";
         std::getline(std::cin, input);
         if (std::cin.eof()) {
-            std::cout << std::endl << "Reached the End-of-File (Ctrl + D). Exiting..." << std::endl;
+            std::cout << std::endl;
+            std::cout << "Reached the End-of-File (Ctrl + D). Exiting...";
+            std::cout << std::endl;
             exit(0);
         }
         if (input.empty())
@@ -151,7 +164,8 @@ void    Contact::setPhone(void) {
             continue;
         input_len = input.length();
         if (!(input_len == 8 || input_len == 10)) {
-            std::cout << "Sorry, the phone must contains 8 or 10 digits." << std::endl;
+            std::cout << "Sorry, the phone must contains 8 or 10 digits.";
+            std::cout << std::endl;
             continue;
         }
         this->m_phone = input;
@@ -166,7 +180,9 @@ void    Contact::setSecret(void) {
         std::cout << "Enter darkest secret : ";
         std::getline(std::cin, input);
         if (std::cin.eof()) {
-            std::cout << std::endl << "Reached the End-of-File (Ctrl + D). Exiting..." << std::endl;
+            std::cout << std::endl;
+            std::cout << "Reached the End-of-File (Ctrl + D). Exiting...";
+            std::cout << std::endl;
             exit(0);
         }
         if (input.empty())
